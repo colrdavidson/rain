74cf4c7ba0bb9deb25f79c8fa1264814c1b321fd
Colin Davidson <colin@pentaquine.com>
2016-06-29 00:29:24 -0700

    Add depth

--- main.c
+++ main.c
@@ -24,10 +24,14 @@ int main() {
 	}
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-	SDL_Surface *bmp_surface = SDL_LoadBMP("assets/sidewalk.bmp");
-	SDL_SetColorKey(bmp_surface, SDL_TRUE, SDL_MapRGB(bmp_surface->format, 0, 0, 0));
-	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, bmp_surface);
-	SDL_FreeSurface(bmp_surface);
+	SDL_Surface *floor_bmp = SDL_LoadBMP("assets/sidewalk.bmp");
+	SDL_Surface *wall_bmp = SDL_LoadBMP("assets/wall.bmp");
+	SDL_SetColorKey(floor_bmp, SDL_TRUE, SDL_MapRGB(floor_bmp->format, 0, 0, 0));
+	SDL_SetColorKey(wall_bmp, SDL_TRUE, SDL_MapRGB(wall_bmp->format, 0, 0, 0));
+	SDL_Texture *floor_tex = SDL_CreateTextureFromSurface(renderer, floor_bmp);
+	SDL_Texture *wall_tex = SDL_CreateTextureFromSurface(renderer, wall_bmp);
+	SDL_FreeSurface(floor_bmp);
+	SDL_FreeSurface(wall_bmp);
 
 
 	u8 running = 1;
@@ -42,24 +46,29 @@ int main() {
 		SDL_RenderClear(renderer);
 
 		i32 box_width, box_height;
-		SDL_QueryTexture(texture, NULL, NULL, &box_width, &box_height);
+		SDL_QueryTexture(floor_tex, NULL, NULL, &box_width, &box_height);
 
 		SDL_Rect dest;
 		dest.w = box_width;
 		dest.h = box_height;
 
 		for (i32 x = 0; x < 25; x++) {
-			for (i32 y = 0; y < 25; y++) {
+			for (i32 y = 25; y > 0; y--) {
 				dest.x = ((x + y) * 16);
 				dest.y = ((x - y) * 8) + (screen_height / 2);
-				SDL_RenderCopy(renderer, texture, NULL, &dest);
+				if ((x % 3) == 0) {
+					SDL_RenderCopy(renderer, floor_tex, NULL, &dest);
+				} else {
+					SDL_RenderCopy(renderer, wall_tex, NULL, &dest);
+				}
 			}
 		}
 
 		SDL_RenderPresent(renderer);
 	}
 
-    SDL_DestroyTexture(texture);
+    SDL_DestroyTexture(floor_tex);
+    SDL_DestroyTexture(wall_tex);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
